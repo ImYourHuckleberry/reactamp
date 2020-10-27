07c46fe84abc014bf36aa86be873f1d592051499
Tyler Krings <w.tyler.krings@gmail.com>
2020-10-27 10:31:13 -0500

    c

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
 import { withAuthenticator } from '@aws-amplify/ui-react'
+import { API, Storage } from 'aws-amplify';
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
@@ -22,11 +23,27 @@ const App = () => {
   }
 
   async function fetchTodos() {
-    try {
-      const todoData = await API.graphql(graphqlOperation(listTodos))
-      const todos = todoData.data.listTodos.items
-      setTodos(todos)
-    } catch (err) { console.log('error fetching todos') }
+    const apiData = await API.graphql({ query: listTodos });
+    const todosFromAPI = apiData.data.listTodos.items;
+    await Promise.all(todosFromAPI.map(async todo => {
+      if (todo.image) {
+        const image = await Storage.get(todo.image);
+        todo.image = image;
+      }
+      return todo;
+    }))
+    setTodos(apiData.data.listTodos.items);
+  }
+
+  async function createTodo() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createTodoMutation, variables: { input: formData } });
+    if (formData.image) {
+      const image = await Storage.get(formData.image);
+      formData.image = image;
+    }
+    setTodos([ ...todos, formData ]);
+    setFormData(initialFormState);
   }
 
   async function addTodo() {
@@ -40,6 +57,14 @@ const App = () => {
       console.log('error creating todo:', err)
     }
   }
+
+  async function onChange(e) {
+  if (!e.target.files[0]) return
+  const file = e.target.files[0];
+  setFormData({ ...formData, image: file.name });
+  await Storage.put(file.name, file);
+  fetchTodos();
+}
 
   return (
     <div style={styles.container}>
@@ -56,15 +81,23 @@ const App = () => {
         value={formState.description}
         placeholder="Description"
       />
+      <input
+  type="file"
+  onChange={onChange}
+/>
       <button style={styles.button} onClick={addTodo}>Create Todo</button>
       {
-        todos.map((todo, index) => (
-          <div key={todo.id ? todo.id : index} style={styles.todo}>
-            <p style={styles.todoName}>{todo.name}</p>
-            <p style={styles.todoDescription}>{todo.description}</p>
-          </div>
-        ))
+  todos.map(todo => (
+    <div key={todo.id || todo.name}>
+      <h2>{todo.name}</h2>
+      <p>{todo.description}</p>
+      <button onClick={() => deleteTodo(todo)}>Delete todo</button>
+      {
+        todo.image && <img src={todo.image} style={{width: 400}} />
       }
+    </div>
+  ))
+}
     </div>
   )
 }
